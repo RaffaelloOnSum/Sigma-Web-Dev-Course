4597fdb558faca9a772c7140339e18da9571e722
RaffaelloOnSum <2ndmarchraffaello@gmail.com>
2024-03-27 07:49:03 +0500

    first commit

---
+++ Bakchodi/script.js
@@ -0,0 +1,8 @@
+function changeHeaderColor() {
+    var header = document.querySelector('header');
+    header.style.backgroundColor = '#ff6347'; // Change header color to coral
+}
+
+function navigateToIndexPage() {
+    window.location.href = "about.html"; // Redirect to index.html
+}
